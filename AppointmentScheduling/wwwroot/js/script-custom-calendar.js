04c0025bbc80424de8db5922985c5b231ebb4c46
Isiah Jones <isiah.jones33@outlook.com>
2021-09-11 19:30:48 -0400

    Configure basic Full Calendar Js

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -1,1 +1,28 @@
-﻿
+﻿$(document).ready(function () {
+    InitializeCalendar();
+});
+var calendar;
+function InitializeCalendar() {
+    try {
+        var calendarElement = document.getElementById('calendar');
+
+        if (calendarElement != null) {
+            calendar = new FullCalendar.Calendar(calendarElement, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                selectable: true,
+                editable: false,
+            });
+            calendar.render();
+        }
+
+    }
+    catch (error) {
+        alert(error);
+    }
+
+}
