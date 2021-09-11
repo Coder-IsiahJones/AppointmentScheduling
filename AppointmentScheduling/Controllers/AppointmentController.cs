466f7def7d5ed034527e45b44d3e38126b1c201c
Isiah Jones <isiah.jones33@outlook.com>
2021-09-11 18:48:01 -0400

    Appointment Controller Created

---
+++ AppointmentScheduling/Controllers/AppointmentController.cs
@@ -0,0 +1,26 @@
+﻿using AppointmentScheduling.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly AppointmentService _appointmentService;
+
+        public AppointmentController(AppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        public IActionResult Index()
+        {
+            _appointmentService.GetDoctorList();
+
+            return View();
+        }
+    }
+}
