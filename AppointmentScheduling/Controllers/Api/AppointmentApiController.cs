f038abd64bf19481db63e108cc4b85e5bf2d3eb8
Isiah Jones <isiah.jones33@outlook.com>
2021-09-11 20:34:35 -0400

    Created controller for api

---
+++ AppointmentScheduling/Controllers/Api/AppointmentApiController.cs
@@ -0,0 +1,34 @@
+﻿using AppointmentScheduling.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Controllers.Api
+{
+    [Route("api/Appointment")]
+    [ApiController]
+    public class AppointmentApiController : Controller
+    {
+        private readonly IAppointmentService _appointmentService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string loginUserId;
+        private readonly string role;
+
+        public AppointmentApiController(IAppointmentService appointmentService, IHttpContextAccessor httpContextAccessor)
+        {
+            _appointmentService = appointmentService;
+            loginUserId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            role = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Role);
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
