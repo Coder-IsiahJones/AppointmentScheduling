5f3494cbe936c6374251521b07680aa5d594af65
Isiah Jones <isiah.jones33@outlook.com>
2021-09-11 20:11:20 -0400

    Add the close & submit buttons.

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -33,3 +33,7 @@ function InitializeCalendar() {
 function onShowModal(obj, isEventDetail) {
     $("#appointmentInput").modal("show");
 }
+
+function onCloseModal() {
+    $("#appointmentInput").modal("hide");
+}
